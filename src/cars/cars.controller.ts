67dc99f99508ae4abc15108ca1d7a06f28e7bf4d
AlejandroMT18 <alejandro.redes18@gmail.com>
2022-08-04 13:24:29 -0600

    Seguimiento hasta la seccion 4 clase 47 ValidationPipe

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,53 +1,64 @@
-import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, Put } from '@nestjs/common';
-import { OutgoingMessage } from 'http';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseUUIDPipe,
+  Patch,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { CarsService } from './cars.service';
+import { createCarDto } from './dto/create-car.dto';
 
 @Controller('cars')
+// Validador a nivel de Controlador
+// @UsePipes(ValidationPipe)
 export class CarsController {
+  constructor(private readonly carsService: CarsService) {}
 
-    constructor (
-        private readonly carsService: CarsService 
-    ) {}
+  @Get()
+  getAllCars() {
+    return {
+      data: this.carsService.findAll(),
+      message: 'successful',
+    };
+  }
 
-    @Get()
-    getAllCars() {
-        return {
-            data:  this.carsService.findAll(),
-            message: 'successful'
-        };
-    }
+  @Get(':id')
+  getCarById(@Param('id', new ParseUUIDPipe({ version: '4' })) id: string) {
+    return {
+      data:
+        this.carsService.findById(id) == null
+          ? {}
+          : this.carsService.findById(id),
+      message:
+        this.carsService.findById(id) == null ? 'Not found' : 'Successful',
+    };
+  }
 
-    @Get(':id')
-    getCarById( @Param('id', ParseIntPipe ) id: number ) {
-        console.log(id);
-        return {
-            data: (this.carsService.findById(id) == null ) ?
-                {}:
-                this.carsService.findById(id),
-            message: (this.carsService.findById(id) == null ) ?
-                'Not found': 
-                'Successful'
-        };
-    }
+  @Post()
+  // Validador a nivel de método
+  // @UsePipes(ValidationPipe)
+  createCar(@Body() car: createCarDto) {
+    return { data: car, message: 'Post' };
+  }
 
-    @Post()
-    createCar( @Body() body: any ) {
-        return{ data: body, message:'Post' };
-    }
+  @Patch(':id')
+  updateCar(
+    @Param('id', new ParseUUIDPipe({ version: '4' })) id: string,
+    @Body() body: any,
+  ) {
+    return {
+      data: body,
+      message: 'Patch',
+    };
+  }
 
-    @Patch(':id')
-    updateCar(
-        @Param('id', ParseIntPipe) id: number,
-        @Body() body: any ) { 
-        return { 
-            data: body,
-            message:'Patch' 
-        };
-    }
-
-    @Delete(':id')
-    deleteCar( @Param('id', ParseIntPipe) id: number ){
-        return{ id_deleted: id, message:'Delete' };
-    }
-
+  @Delete(':id')
+  deleteCar(@Param('id', new ParseUUIDPipe({ version: '4' })) id: string) {
+    return { id_deleted: id, message: 'Delete' };
+  }
 }
